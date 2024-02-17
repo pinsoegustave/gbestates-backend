a0d6a23dcb0ab61e8f26367b95b97782ea7d1c81
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-02-17 18:00:10 +0200

    user route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { deleteUser, getUserListings, test, updateUser } from '../controllers/user.controller.js';
-import { verifyToken } from '../utils/VerifyUser.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
